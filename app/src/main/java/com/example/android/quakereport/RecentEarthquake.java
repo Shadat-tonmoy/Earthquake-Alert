4e65e728eed3e3f08864a92449a72e2fcd2115d0
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2017-11-11 13:02:44 +0600

    no internet connectivity and empty data set UI implemented

--- app/src/main/java/com/example/android/quakereport/RecentEarthquake.java
+++ app/src/main/java/com/example/android/quakereport/RecentEarthquake.java
@@ -1,6 +1,8 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.net.MalformedURLException;
@@ -26,8 +29,10 @@ public class RecentEarthquake extends Fragment implements LoaderManager.LoaderCa
     private ListView earthquakeListView;
     ArrayList<Earthquake> earthquakes;
     EarthquakeAdapter adapter;
-    private View view;
+    private View view,emptyView,noInternetConnectionView;
     private TextView debugView;
+    private ProgressBar progressBar;
+    private TextView retryButton;
 
 
 
@@ -45,6 +50,10 @@ public class RecentEarthquake extends Fragment implements LoaderManager.LoaderCa
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_recent_earthquake, container, false);
+        emptyView = view.findViewById(R.id.nothing_found_msg);
+        noInternetConnectionView = view.findViewById(R.id.no_internet_msg);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        retryButton = (TextView) view.findViewById(R.id.retryButton);
         return view;
     }
 
@@ -56,10 +65,16 @@ public class RecentEarthquake extends Fragment implements LoaderManager.LoaderCa
         earthquakeListView = (ListView) view.findViewById(R.id.list);
         adapter = new EarthquakeAdapter(getActivity().getApplicationContext(),R.layout.single_row,R.id.magnitude,earthquakes);
         earthquakeListView.setAdapter(adapter);
+        emptyView.setVisibility(View.GONE);
+        noInternetConnectionView.setVisibility(View.GONE);
         //debugView = (TextView) view.findViewById(R.id.debug);
-
-
-        getActivity().getSupportLoaderManager().initLoader(1,null,this).forceLoad();
+        getData();
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getData();
+            }
+        });
     }
 
 
@@ -73,6 +88,8 @@ public class RecentEarthquake extends Fragment implements LoaderManager.LoaderCa
 
     @Override
     public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> data) {
+        progressBar.setVisibility(View.GONE);
+        noInternetConnectionView.setVisibility(View.GONE);
         Log.e("Loading is finished","Yes Man!!!");
         for(int i=0;i<data.size();i++)
         {
@@ -81,13 +98,31 @@ public class RecentEarthquake extends Fragment implements LoaderManager.LoaderCa
             Log.e("Result : ",result);
         }
         adapter.setEarthquakes((ArrayList<Earthquake>) data);
-
-
+        earthquakeListView.setEmptyView(emptyView);
     }
 
     @Override
     public void onLoaderReset(Loader<List<Earthquake>> loader) {
         earthquakes = new ArrayList<Earthquake>();
 
+    }
+
+    public void getData()
+    {
+        progressBar.setVisibility(View.VISIBLE);
+        noInternetConnectionView.setVisibility(View.GONE);
+        ConnectivityManager manager = (ConnectivityManager)getActivity().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        if(networkInfo!=null && networkInfo.isConnected())
+        {
+            getActivity().getSupportLoaderManager().initLoader(1,null,this).forceLoad();
+        }
+        else
+        {
+            progressBar.setVisibility(View.GONE);
+            earthquakeListView.setEmptyView(noInternetConnectionView);
+        }
     }
 }
